22bda859c4012421e7bed1c54aaecb7cbb9322bf
Manh Pham <manhvamanh12@gmail.com>
2022-03-07 12:55:50 +0700

    commit2

--- KIemTra/KIemTra/Form1.Designer.cs
+++ KIemTra/KIemTra/Form1.Designer.cs
@@ -32,7 +32,7 @@ namespace KIemTra
             this.label1 = new System.Windows.Forms.Label();
             this.txtusername = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -56,23 +56,23 @@ namespace KIemTra
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(96, 176);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.Size = new System.Drawing.Size(66, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Password";
+            this.label2.Text = "Mật khẩu";
             // 
-            // txtpassword
+            // txtmatkhau
             // 
-            this.txtpassword.Location = new System.Drawing.Point(231, 176);
-            this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(231, 22);
-            this.txtpassword.TabIndex = 3;
+            this.txtmatkhau.Location = new System.Drawing.Point(231, 176);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(231, 22);
+            this.txtmatkhau.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtmatkhau);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtusername);
             this.Controls.Add(this.label1);
@@ -88,7 +88,7 @@ namespace KIemTra
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtusername;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtmatkhau;
     }
 }
 
