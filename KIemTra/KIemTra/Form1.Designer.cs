006f369afcfb84718cd91516d4e64934b57ce608
Manh Truong <truongnguyen0777@gmail.com>
2022-03-07 12:50:48 +0700

    commit

--- KIemTra/KIemTra/Form1.Designer.cs
+++ KIemTra/KIemTra/Form1.Designer.cs
@@ -29,13 +29,44 @@ namespace KIemTra
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "username";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(231, 67);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(231, 22);
+            this.txtusername.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtusername;
     }
 }
 
